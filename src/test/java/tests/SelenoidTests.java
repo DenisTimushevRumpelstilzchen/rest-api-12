c28b808304124fa34a01a7191aee8e085d5b9df1
Your NameDenis <tdv14011984@gmail.com>
2022-06-20 13:52:58 +0300

    add tests

--- src/test/java/tests/SelenoidTests.java
+++ src/test/java/tests/SelenoidTests.java
@@ -2,8 +2,11 @@ package tests;
 
 import org.junit.jupiter.api.Test;
 
+import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.given;
+
 import static org.hamcrest.Matchers.is;
+
 
 public class SelenoidTests {
     /*
